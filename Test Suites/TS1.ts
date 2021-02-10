80b5f367dcc97dfc4ac6836c22f30e0d90269b66
ALODIA <ALODIA@DESKTOP-3KGOLD0>
2021-02-10 13:05:14 +0800

    Test

---
+++ Test Suites/TS1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>714b63fe-b43c-44cc-b0f6-7654dfc8d72c</testSuiteGuid>
+   <testCaseLink>
+      <guid>83b9a87e-7ace-4fc4-a56d-3a5429be71b0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
